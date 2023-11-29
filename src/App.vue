b20185304944ef0fcc8591381aa5986904fd0bc2
mauroluciano <mauroluciano911@gmail.com>
2023-11-29 15:47:46 +0100

    add appCard

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import axios from "axios";
+import AppCard from "./components/AppCard.vue";
+
+export default {
+  components: {
+    AppCard,
+  },
+
+  created() {
+    axios.get("http://localhost:8000/api/apartments").then((response) => {
+      console.log(response.data.data);
+    });
+  },
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <AppCard />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
---
+++ src/components/AppList.vue
@@ -0,0 +1,29 @@
+<script>
+import AppCard from "./components/AppCard.vue";
+
+export default {
+  data() {
+    return {
+      title: "Hello guest",
+    };
+  },
+
+  props: {
+    apartments: Array,
+  },
+
+  components: {
+    AppCard,
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <h2>AppList</h2>
+    <div class="row row-cols-3 g-4">
+      <AppCard v-for="apartment in apartments" :apartment="apartment" />
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped></style>
